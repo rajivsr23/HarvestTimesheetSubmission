a67b54980fb0a67b7e9b3c69c40622dd348eed81
Ricky Raghavan <rajivsr@yahoo.com>
2018-01-19 16:55:51 -0500

    Working-User Entries, Checks Approved

--- Harvest_Lock_Hours_Phase2.php
+++ Harvest_Lock_Hours_Phase2.php
@@ -36,12 +36,43 @@ $today_date=date("d-m",$today);
 echo $today_date;
 
 
+$startLastMonth = mktime(0, 0, 0, date("m") - 2, 1, date("Y"));
+$endLastMonth = mktime(0, 0, 0, date("m") -1, 0, date("Y"));
+
+$unixdate_1=strtotime($ThreeWeeks);
+$unixdate_2=strtotime($TwoWeeks);
+
+$range_test= new Harvest_Range ( $unixdate_1, $unixdate_2);
+
+$range=Harvest_Range::lastMonth("EST",Harvest_Range::MONDAY);
+//Getting the List of Users in order to extract the User ID
 
 
+$users=$api->getUsers();
+	$users_Data=$users->data;
 
+foreach ($users_Data as $key => $value) {
+	$user_id=$value->get("id");
 
-$range= new Harvest_Range ( $ThreeWeeks, $TwoWeeks);
+//Calling the User Entries Method to get the is-closed property
+$user_entries=$api->getUserEntries($user_id,$range);	
+$user_entries_data=$user_entries->data;
 
+echo "<br>User ID: ".$user_id;
+
+var_dump($user_entries_data);
+
+foreach ($user_entries_data as $key2 => $value2) {
+
+$approved=$value2->get("is-closed");
+
+if($approved=="false"){
+
+	echo "<br>Timesheet Not Approved";
+}
+}
+
+}
 
 
 ?>
